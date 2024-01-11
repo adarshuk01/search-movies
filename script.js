5394aa1595ca6ffe17be0852d08e99af1d9acf93
adarsh u k <90322570+adarshuk01@users.noreply.github.com>
2024-01-11 21:31:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let result = document.getElementById("result");
 
 let getMovie = () => {
   let movieName = movieNameRef.value;
-  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
   if (movieName.length <= 0) {
     result.innerHTML = `<h4 class="msg">Please enter a movie name !</h4>`;
   } else {
